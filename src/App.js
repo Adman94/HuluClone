131fddbc506e3a8578db0b96b0ac9782ad7d3866
adman94 <adman00794@gmail.com>
2020-08-26 02:18:15 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header'
+import Results from './Results'
+import Nav from './Nav';
+import requests from './requests'
 
 function App() {
+  
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending)
   return (
     <div className="app">
       <Header />
+      <Nav setSelectedOption={setSelectedOption} />
+      <Results selectedOption={selectedOption} />
     </div>
   );
 }
